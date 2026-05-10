b699aaef5ff620618e46507221b3eda9a8e9aa23
Alexis Castellanos <castellanos.alexis@hotmail.com>
2026-05-10 12:57:47 -0600

    feat 10: interfaces y enumeraciones

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import './style.css'
 // import './bases/04-arrays'
 // import './bases/05-functions'
 // import './bases/06-obj-destructuring'
-import './bases/07-array-destructuring'
+// import './bases/07-array-destructuring'
+import './bases/data/heroes.data'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <div>Hello world</div>
