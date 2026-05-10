f1e14d4aa613f8fc3ff3f13ee5f8fa701594660f
Alexis Castellanos <castellanos.alexis@hotmail.com>
2026-05-10 00:14:38 -0600

    feat 1:  variables y constantes

---
+++ src/bases/01-const-let.ts
@@ -0,0 +1,10 @@
+const firstName: string = "John";
+let lastName = "Doe";
+
+let diceNumber = 6;
+diceNumber = 3;
+
+const containsLeter = lastName.includes("D")
+
+console.log("🚀 :", { firstName, lastName, diceNumber, containsLeter })
+
