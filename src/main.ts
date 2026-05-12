7d1ad2565e02c32fb777daf58adc47ad89269be3
Alexis Castellanos <castellanos.alexis@hotmail.com>
2026-05-11 20:22:58 -0600

    feat 15: async/await

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,8 @@ import './style.css'
 // import './bases/08-imp-exp'
 // import './bases/09-promises'
 // import { Owner } from './bases/data/heroes.data'
-import './bases/10-fetch-api'
+// import './bases/10-fetch-api'
+import './bases/11-async-await'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <div>Hello world</div>
